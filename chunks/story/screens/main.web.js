d2304dbd1e9422fdedbe5eeaf250b00bc7bf1a85
I. Dan Calinescu <idancalinescu@gmail.com>
2018-03-13 09:12:40 +0200

    now loading posts index remotly
    Former-commit-id: 83ec90be5bbdc0ed4daafea2245e11faa6db9c0c

--- chunks/story/screens/main.web.js
+++ chunks/story/screens/main.web.js
@@ -1,33 +1,63 @@
 import React from 'react'
 import { Screen, Components } from 'react-dom-chunky'
+import { LinearProgress } from 'rmwc/LinearProgress'
+import {
+  Card,
+  CardMedia,
+  CardMediaItem,
+  CardPrimary,
+  CardTitle,
+  CardActions,
+  CardActionButtons,
+  CardAction,
+  CardPrimaryAction,
+  CardActionIcons,
+  CardSubtitle,
+  CardSupportingText,
+  CardHorizontalBlock
+} from 'rmwc/Card'
+import { Typography } from 'rmwc/Typography'
+import {
+  ListDivider
+} from 'rmwc/List'
 
 export default class CategoriesScreen extends Screen {
 
   constructor (props) {
     super(props)
-    this.state = { ...this.state }
+    this.state = { ...this.state, loading: true }
   }
 
   componentDidMount () {
     super.componentDidMount()
-    this._posts = this.importData('posts')
+    this.importRemoteData(this.props.index).then(posts => {
+      this.setState({ posts, loading: false })
+    })
   }
 
-  itemSelected (fullPath) {
-    console.log(fullPath)
-    // this.triggerRedirect(fullPath)
-  }
+  renderContent () {
+    if (this.state.loading) {
+      const width = '100%'
+      return <div style={{ display: 'flex', flex: 1, margin: '10px', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }} >
+        <Card style={{width, margin: '20px', padding: '0px'}} >
+          <Typography use='title' tag='h1'>
+            Loading ... Just a sec.
+          </Typography>
+          <ListDivider />
+          <LinearProgress determinate={false} />
+        </Card>
+      </div>
+    }
 
-  get posts () {
-    return this._posts || []
+    return <Components.Collection
+      id='posts'
+      categories={this.state.posts}
+    />
   }
 
   components () {
     return [
-      <Components.Collection
-        id='posts'
-        categories={this.posts}
-      />
+      this.renderContent()
     ]
   }
 }
