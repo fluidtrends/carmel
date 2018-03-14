29df4cd4d41359f2d74896fe6c4079d4a7b7885f
AndyComan <andy@evanghelic.ro>
2018-03-14 15:41:17 +0200

    Added challenges chunk
    Former-commit-id: 3b4217ffdb80a8d9be0df8a1d854ff8ff6cd0ab1

--- chunks/index.web.js
+++ chunks/index.web.js
@@ -1,6 +1,7 @@
 // AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
 export { default as story } from './story/index.web'
+export { default as challenges } from './challenges/index.web'
 export { default as tokens } from './tokens/index.web'
 export { default as intro } from './intro/index.web'
 export { default as dashboard } from './dashboard/index.web'
