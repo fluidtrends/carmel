908ec4fbdb2aacc4d1c327bf6e6f21403f32fafc
AndyComan <andy@evanghelic.ro>
2018-03-27 15:33:13 +0300

    Add initial airdrop component, fix metamask bug

--- chunks/dashboard/components/login.js
+++ chunks/dashboard/components/login.js
@@ -25,8 +25,7 @@ import { Typography } from 'rmwc/Typography'
 import { FormField } from 'rmwc/FormField'
 
 export default class LoginComponent extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props)
     this._done = this.done.bind(this)
     this._register = this.register.bind(this)
@@ -35,102 +34,158 @@ export default class LoginComponent extends Component {
     this.state = { ...super.state }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     super.componentDidMount()
   }
 
-  done () {
+  done() {
     if (!this.state.email) {
       this.setState({ error: errors.email, errorType: 'email', loading: false })
       return
     }
 
     if (!this.state.password) {
-      this.setState({ error: errors.password, errorType: 'password', loading: false })
+      this.setState({
+        error: errors.password,
+        errorType: 'password',
+        loading: false
+      })
       return
     }
 
     this.setState({ loading: true })
     this.props.signIn({
       email: this.state.email,
-      password: this.state.password })
+      password: this.state.password
+    })
   }
 
-  register () {
+  register() {
     this.props.onRegister && this.props.onRegister()
   }
 
-  resetPassword () {
+  resetPassword() {
     this.props.onResetPassword && this.props.onResetPassword()
   }
 
-  get error () {
-    return (this.state.error ? this.state.error : this.props.error)
+  get error() {
+    return this.state.error ? this.state.error : this.props.error
   }
 
-  renderError () {
+  renderError() {
     if (!this.error) {
       return
     }
 
     return (
-      <div style={{margin: '20px', color: '#ef5350', textAlign: 'center'}}>
-        { this.error }
+      <div style={{ margin: '20px', color: '#ef5350', textAlign: 'center' }}>
+        {this.error}
       </div>
     )
   }
 
-  renderLoading () {
+  renderLoading() {
     if (!this.state.loading || this.error) {
       return
     }
-    return <div>
-      <LinearProgress determinate={false} />
-    </div>
+    return (
+      <div>
+        <LinearProgress determinate={false} />
+      </div>
+    )
   }
 
   onKeyPress(event) {
-    if(event.key == 'Enter') {
+    if (event.key == 'Enter') {
       this._done()
     }
   }
 
-  render () {
+  render() {
     const width = this.props.isSmallScreen ? '95vw' : '600px'
     const padding = this.props.isSmallScreen ? '2px' : '30px'
-    return (<div style={{ display: 'flex', flex: 1, margin: '40px', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }} >
-      <Card style={{width, margin: '10px', padding }} >
-        <div style={{padding: '4px'}}>
-          <Typography use='title' tag='h2'> Welcome back </Typography>
-        </div>
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flex: 1,
+          margin: '40px',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <Card style={{ width, margin: '10px', padding }}>
+          <div style={{ padding: '4px' }}>
+            <Typography use="title" tag="h2">
+              {' '}
+              Welcome back{' '}
+            </Typography>
+          </div>
 
-        { this.renderError() }
-        { this.renderLoading() }
-        <TextField disabled={this.state.loading && !this.error} outlined withLeadingIcon='email' label={this.state.email ? '' : 'Enter your email address'}
-          onChange={(val) => this.setState({ email: val.target.value, error: '' })}
-          onKeyPress={this.onKeyPress} />
+          {this.renderError()}
+          {this.renderLoading()}
+          <TextField
+            disabled={this.state.loading && !this.error}
+            outlined
+            withLeadingIcon="email"
+            label={this.state.email ? '' : 'Enter your email address'}
+            onChange={val =>
+              this.setState({ email: val.target.value, error: '' })
+            }
+            onKeyPress={this.onKeyPress}
+          />
 
-        <TextField type='password' disabled={this.state.loading && !this.error} outlined withLeadingIcon='lock' label={this.state.password ? '' : 'Enter your password'}
-          onChange={(val) => this.setState({ password: val.target.value, error: '' })}
-          onKeyPress={this.onKeyPress} />
+          <TextField
+            type="password"
+            disabled={this.state.loading && !this.error}
+            outlined
+            withLeadingIcon="lock"
+            label={this.state.password ? '' : 'Enter your password'}
+            onChange={val =>
+              this.setState({ password: val.target.value, error: '' })
+            }
+            onKeyPress={this.onKeyPress}
+          />
 
-        <CardActions style={{justifyContent: 'center', margin: '20px'}}>
-          <CardActionButtons>
-            <Button disabled={this.state.loading && !this.error} raised onClick={this._done} theme='secondary-bg text-primary-on-secondary'> Sign In</Button>
-          </CardActionButtons>
-        </CardActions>
-        <CardActions style={{justifyContent: 'center', margin: '0px'}}>
-          <CardActionButtons>
-            <Button disabled={this.state.loading && !this.error} onClick={this._register}> Need an account? </Button>
-          </CardActionButtons>
-        </CardActions>
-        <CardActions style={{justifyContent: 'center', margin: '0px'}}>
-          <CardActionButtons>
-            <Button disabled={this.state.loading && !this.error} onClick={this._resetPassword}> Forgot your password? </Button>
-          </CardActionButtons>
-        </CardActions>
-      </Card>
-    </div>)
+          <CardActions style={{ justifyContent: 'center', margin: '20px' }}>
+            <CardActionButtons>
+              <Button
+                disabled={this.state.loading && !this.error}
+                raised
+                onClick={this._done}
+                theme="secondary-bg text-primary-on-secondary"
+              >
+                {' '}
+                Sign In
+              </Button>
+            </CardActionButtons>
+          </CardActions>
+          <CardActions style={{ justifyContent: 'center', margin: '0px' }}>
+            <CardActionButtons>
+              <Button
+                disabled={this.state.loading && !this.error}
+                onClick={this._register}
+              >
+                {' '}
+                Need an account?{' '}
+              </Button>
+            </CardActionButtons>
+          </CardActions>
+          <CardActions style={{ justifyContent: 'center', margin: '0px' }}>
+            <CardActionButtons>
+              <Button
+                disabled={this.state.loading && !this.error}
+                onClick={this._resetPassword}
+              >
+                {' '}
+                Forgot your password?{' '}
+              </Button>
+            </CardActionButtons>
+          </CardActions>
+        </Card>
+      </div>
+    )
   }
 }
 
