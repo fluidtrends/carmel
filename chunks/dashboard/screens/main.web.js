22d41f4749dc206d0099fd79bbf6928fea185091
AndyComan <andy@evanghelic.ro>
2018-03-27 09:41:53 +0300

    Fix logout on mobile

--- chunks/dashboard/screens/main.web.js
+++ chunks/dashboard/screens/main.web.js
@@ -18,12 +18,10 @@ import {
   CardHorizontalBlock
 } from 'rmwc/Card'
 import { Typography } from 'rmwc/Typography'
-import {
-  ListDivider
-} from 'rmwc/List'
+import { ListDivider } from 'rmwc/List'
 
 export default class MainDashboardScreen extends Screen {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = { ...this.state, loading: false }
     this._onSectionSelect = this.onSectionSelect.bind(this)
@@ -35,7 +33,7 @@ export default class MainDashboardScreen extends Screen {
     this._levelUp = this.levelUp.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     super.componentDidMount()
     this._sideMenu = [].concat(this.menu)
 
@@ -44,60 +42,71 @@ export default class MainDashboardScreen extends Screen {
     }
   }
 
-  get sideMenu () {
+  get sideMenu() {
     return this._sideMenu
   }
 
-  register () {
+  register() {
     this.setState({ register: true, resetPassword: false })
   }
 
-  reset () {
+  reset() {
     this.setState({ resetPassword: true })
   }
 
-  login () {
+  login() {
     this.setState({ register: false, resetPassword: false })
   }
 
-  loadDashboard (account) {
+  loadDashboard(account) {
     this.saveAuth(account).then(() => {
       this.loadSections(account)
     })
   }
 
-  renderLogin () {
-    return <ChunkComponents.Login
-      error={this.state.loginError}
-      signIn={this.props.signIn}
-      onRegister={this._register}
-      onResetPassword={this._reset} />
+  renderLogin() {
+    return (
+      <ChunkComponents.Login
+        error={this.state.loginError}
+        signIn={this.props.signIn}
+        onRegister={this._register}
+        onResetPassword={this._reset}
+      />
+    )
   }
 
-  renderRegister () {
-    return <ChunkComponents.Register
-      error={this.state.registerError}
-      signUp={this.props.signUp}
-      onLogin={this._login} />
+  renderRegister() {
+    return (
+      <ChunkComponents.Register
+        error={this.state.registerError}
+        signUp={this.props.signUp}
+        onLogin={this._login}
+      />
+    )
   }
 
-  renderResetPassword () {
-    return <ChunkComponents.ResetPassword error={this.state.registerError} onLogin={this._login} />
+  renderResetPassword() {
+    return (
+      <ChunkComponents.ResetPassword
+        error={this.state.registerError}
+        onLogin={this._login}
+      />
+    )
   }
 
-  didRegister () {
+  didRegister() {
     this.setState({ register: false })
   }
 
-  registerError (error) {
+  registerError(error) {
     this.setState({ registerError: error.message })
   }
 
-  signInError (error) {
+  signInError(error) {
     this.setState({ loginError: error.message })
   }
 
-  dashboardAction (action, props) {
+  dashboardAction(action, props) {
     switch (action) {
       case 'installProvider':
       case 'transactionDetails':
@@ -110,18 +119,20 @@ export default class MainDashboardScreen extends Screen {
     }
   }
 
-  levelUp (data) {
+  levelUp(data) {
     this.setState({ loading: true })
 
     setTimeout(() => {
-      this.props.newLevelUp(Object.assign({}, data, {
-        userId: this.props.account._id,
-        userEmail: this.props.account.email
-      }))
+      this.props.newLevelUp(
+        Object.assign({}, data, {
+          userId: this.props.account._id,
+          userEmail: this.props.account.email
+        })
+      )
     }, 300)
   }
 
-  levelUpDone (data) {
+  levelUpDone(data) {
     const request = {
       nodeName: 'levelups',
       node: data,
@@ -141,16 +152,15 @@ export default class MainDashboardScreen extends Screen {
     }, 300)
   }
 
-  levelUpHistoryDone (data) {
-  }
+  levelUpHistoryDone(data) {}
 
-  updatedAccount () {
+  updatedAccount() {
     setTimeout(() => {
       this.props.getAccount()
     }, 300)
   }
 
-  loadSections (account) {
+  loadSections(account) {
     this._sections = this.importData('sections')
 
     if (!this.sections || this.sections.length === 0) {
@@ -161,7 +171,11 @@ export default class MainDashboardScreen extends Screen {
 
     this.sections.forEach(s => {
       this._sideMenu.push({
-        id: `extended-${s.path}`, icon: 'home', title: s.title, path: s.path
+        id: `extended-${s.path}`,
+        action: s.action,
+        icon: 'home',
+        title: s.title,
+        path: s.path
       })
     })
 
@@ -182,11 +196,11 @@ export default class MainDashboardScreen extends Screen {
     this.setState({ section, loading: false, account })
   }
 
-  get sections () {
+  get sections() {
     return this._sections || []
   }
 
-  onSectionSelect (section) {
+  onSectionSelect(section) {
     if (section.action && this[section.action]) {
       this[section.action]()
       return
@@ -195,18 +209,29 @@ export default class MainDashboardScreen extends Screen {
     this.triggerRedirect(`${this.props.path}/${section.path}`)
   }
 
-  renderScreenContent () {
+  renderScreenContent() {
     if (this.state.loading) {
       const width = this.props.compact ? '95vw' : '600px'
-      return <div style={{ display: 'flex', flex: 1, margin: '10px', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }} >
-        <Card style={{width, margin: '20px', padding: '0px'}} >
-          <Typography use='title' tag='h1'>
-            Processing ... Just a sec.
-          </Typography>
-          <ListDivider />
-          <LinearProgress determinate={false} />
-        </Card>
-      </div>
+      return (
+        <div
+          style={{
+            display: 'flex',
+            flex: 1,
+            margin: '10px',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Card style={{ width, margin: '20px', padding: '0px' }}>
+            <Typography use="title" tag="h1">
+              Processing ... Just a sec.
+            </Typography>
+            <ListDivider />
+            <LinearProgress determinate={false} />
+          </Card>
+        </div>
+      )
     }
 
     if (!this.isLoggedIn) {
@@ -219,35 +244,47 @@ export default class MainDashboardScreen extends Screen {
     return this.renderSections()
   }
 
-  renderSectionContent () {
-    if (this.state.section.component && ChunkComponents[this.state.section.component]) {
+  renderSectionContent() {
+    if (
+      this.state.section.component &&
+      ChunkComponents[this.state.section.component]
+    ) {
       const SectionComponent = ChunkComponents[this.state.section.component]
-      var sectionProps = Object.assign({}, {
-        account: this.account,
-        browser: this.browser,
-        onAction: this._dashboardAction,
-        compact: this.isSmallScreen,
-        updates: this.props.updates })
-      return <div style={{ }}><SectionComponent {...sectionProps} />
-      </div>
+      var sectionProps = Object.assign(
+        {},
+        {
+          account: this.account,
+          browser: this.browser,
+          onAction: this._dashboardAction,
+          compact: this.isSmallScreen,
+          updates: this.props.updates
+        }
+      )
+      return (
+        <div style={{}}>
+          <SectionComponent {...sectionProps} />
+        </div>
+      )
     }
     return <div />
   }
 
-  renderSections () {
-    return <Components.Dashboard
-      compact={this.isSmallScreen}
-      renderContent={this._renderSectionContent}
-      sectionsBackgroundColor='#FAFAFA'
-      sectionColor='#B0BEC5'
-      sectionSelectedColor='#039BE5'
-      sections={this.sections}
-      section={this.state.section}
-      onSectionSelect={this._onSectionSelect}
-    />
+  renderSections() {
+    return (
+      <Components.Dashboard
+        compact={this.isSmallScreen}
+        renderContent={this._renderSectionContent}
+        sectionsBackgroundColor="#FAFAFA"
+        sectionColor="#B0BEC5"
+        sectionSelectedColor="#039BE5"
+        sections={this.sections}
+        section={this.state.section}
+        onSectionSelect={this._onSectionSelect}
+      />
+    )
   }
 
-  components () {
+  components() {
     return [this.renderScreenContent()]
   }
 }
