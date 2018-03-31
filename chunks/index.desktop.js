2d5ff264d09f8c42b3f99f2c27c96bfad0e25877
I. Dan Calinescu <idancalinescu@gmail.com>
2018-03-31 06:46:19 +0300

    added studio start and profile screen

--- chunks/index.desktop.js
+++ chunks/index.desktop.js
@@ -1,3 +1,4 @@
 // AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
 export { default as intro } from './intro/index.desktop'
+export { default as dashboard } from './dashboard/index.desktop'
