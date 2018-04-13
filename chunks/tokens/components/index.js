aa1a12492cd7edf91d2eee1e1c087a6186cba656
I. Dan Calinescu <idancalinescu@gmail.com>
2018-04-13 18:33:01 +0300

    added tokens public buy widget - still in progress

---
+++ chunks/tokens/components/index.js
@@ -0,0 +1,3 @@
+export { default as Picker } from './picker'
+export { default as Footer } from './footer'
+export { default as Header } from './header'
--- chunks/tokens/screens/main.web.js
+++ chunks/tokens/screens/main.web.js
@@ -1,35 +1,14 @@
 import React from 'react'
-import { Screen, Components } from 'react-dom-chunky'
-import {
-  Card,
-  CardMedia,
-  CardMediaItem,
-  CardPrimary,
-  CardTitle,
-  CardActions,
-  CardActionButtons,
-  CardAction,
-  CardPrimaryAction,
-  CardActionIcons,
-  CardSubtitle,
-  CardSupportingText,
-  CardHorizontalBlock
-} from 'rmwc/Card'
-import { Button, ButtonIcon } from 'rmwc/Button'
-import { Typography } from 'rmwc/Typography'
-import { FormField } from 'rmwc/FormField'
-import { Chip, ChipText, ChipIcon, ChipSet } from 'rmwc/Chip'
-import { ListDivider } from 'rmwc/List'
-import { Fab } from 'rmwc/Fab'
-import { Icon } from 'rmwc/Icon'
+import { Screen } from 'react-dom-chunky'
+import { Picker } from '../components'
 
 export default class MainTokensScreen extends Screen {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = { ...this.state }
   }
 
-  componentDidMount() {
+  componentDidMount () {
     super.componentDidMount()
 
     if (this.isLoggedIn) {
@@ -37,11 +16,7 @@ export default class MainTokensScreen extends Screen {
     }
   }
 
-  get features() {
-    return []
-  }
-
-  components() {
-    return super.components().concat(this.features)
-  }
+  // components () {
+  //   return [<Picker />]
+  // }
 }
