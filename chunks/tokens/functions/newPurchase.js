6143af49daec586768d38614a222bfc9d7dc53c4
I. Dan Calinescu <idancalinescu@gmail.com>
2018-04-25 03:28:39 +0300

    added tokens purchases function - in progress

---
+++ chunks/tokens/functions/newPurchase.js
@@ -0,0 +1,53 @@
+const chunky = require('react-cloud-chunky')
+
+function getUser (email) {
+  return chunky.firebase.operation('retrieve', {
+    key: 'users',
+    orderBy: 'email',
+    equalTo: email
+  })
+  .then(data => {
+    if (!data || data.length === 0) {
+      throw new Error('No user')
+    }
+    return data
+  })
+}
+
+function createMemberPurchase (user, purchase) {
+  return chunky.firebase.operation('add', Object.assign({}, {
+    node: 'purchases',
+    status: 'pending'
+  }, purchase, {
+    from: user.email,
+    join: {
+      users: {
+        id: user._id
+      }
+    }
+  }))
+}
+
+function createGuestPurchase (email, purchase) {
+  return chunky.firebase.operation('create', Object.assign({}, {
+    node: 'purchases',
+    status: 'pending',
+    email
+  }, purchase))
+}
+
+function main (event, chunk, config) {
+  return new Promise((resolve, reject) => {
+    chunky.firebase.initialize(config.google)
+
+    getUser(event.body.email)
+    .then((user) => createMemberPurchase(user, event.body.data))
+    .then(() => resolve({ message: 'Member purchase pending' }))
+    .catch(() => {
+      createGuestPurchase(event.body.email, event.body.data)
+      .then(() => resolve({ message: 'Guest purchase pending' }))
+    })
+  })
+}
+
+module.exports.main = chunky.handler(main, __filename)
