1220a38c3fcc9e7b88f9b7c584a2fa181f75f4ba
I. Dan Calinescu <idancalinescu@gmail.com>
2018-04-25 15:54:06 +0300

    simplified functions
    Former-commit-id: 13b6b198a8740e79150c019fd80d97f915992bdb

--- chunks/communications/functions/sendEmail.js
+++ chunks/communications/functions/sendEmail.js
@@ -1,48 +1,12 @@
 const chunky = require('react-cloud-chunky')
 
-function createRequest ({ to, from, subject, text, html }) {
-  return ({
-    Destination: {
-      ToAddresses: to
-    },
-    Message: {
-      Body: {
-        Html: {
-          Charset: 'UTF-8',
-          Data: html
-        },
-        Text: {
-          Charset: 'UTF-8',
-          Data: text
-        }
-      },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: subject
-      }
-    },
-    ReturnPath: from,
-    Source: from
-  })
-}
-
 function main ({ event, chunk, config }) {
-  return new Promise((resolve, reject) => {
-    const request = createRequest({
-      to: config.settings.adminEmails,
-      from: 'team@carmel.io',
-      subject: 'Carmel Notification',
-      text: event.body.text,
-      html: event.body.html
-    })
-
-    chunky.aws.ses.sendEmail(request, (error, data) => {
-      if (error) {
-        resolve({ sent: false })
-        return
-      }
-      resolve({ sent: true })
-    })
+  return chunky.emailer.send({
+    to: config.settings.adminEmails,
+    from: 'team@carmel.io',
+    subject: 'Carmel Notification',
+    text: event.body.text,
+    html: event.body.html
   })
 }
 
