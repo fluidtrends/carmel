97e4db474e195016ae3abb84f3e4ac8351714fcb
I. Dan Calinescu <idancalinescu@gmail.com>
2018-05-19 23:59:39 +0300

    added telegram and twitter to the account
    Former-commit-id: 068f69e780d9d1834e8439f7c3039c9d93279e4c

--- chunks/social/functions/twitter.js
+++ chunks/social/functions/twitter.js
@@ -6,20 +6,22 @@ const auth = { limit: 1 }
 
 function executor ({ event, chunk, config, account }) {
   const client = new Twitter({
-    consumer_key: config.twitter.mainApp.apiKey,
-    consumer_secret: config.twitter.mainApp.apiSecret,
-    access_token_key: config.twitter.mainApp.accessToken,
-    access_token_secret: config.twitter.mainApp.tokenSecret
+    consumer_key: config.settings.twitter.mainApp.apiKey,
+    consumer_secret: config.settings.twitter.mainApp.apiSecret,
+    access_token_key: config.settings.twitter.mainApp.accessToken,
+    access_token_secret: config.settings.twitter.mainApp.tokenSecret
   })
 
   return new Promise((resolve, reject) => {
     const params = { screen_name: 'carmelplatform' }
-    client.get('followers/ids', params, (error, tweets, response) => {
+    client.get('followers/list', params, (error, tweets, response) => {
       if (error) {
         reject(error)
         return
       }
-      resolve({ tweets, response })
+
+      const users = tweets.users
+      resolve({ users })
     })
   })
 }
