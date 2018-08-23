e2a0f24d07a7c4ef5c3593fb136e6f5dd24b39aa
I. Dan Calinescu <idancalinescu@gmail.com>
2018-08-23 10:05:50 +0300

    only use cwd when running a command

--- desktop/commands/index.js
+++ desktop/commands/index.js
@@ -2,11 +2,9 @@ const path = require('path')
 const { fork } = require('child_process')
 const { system } = require('../utils')
 
-// require('app-module-path').addPath(path.resolve(system.CARMEL_ROOT, 'node_modules'))
-
-const cwd = path.resolve(system.CARMEL_HOME, '.cache', 'desktop')
-
 const eventHandler = (type) => (event, mainWindow, session, props) => {
+  const cwd = path.resolve(system.CARMEL_HOME, '.cache', 'desktop')
+  process.chdir(cwd)
   const processFile = path.resolve(system.CARMEL_ROOT, 'desktop', 'commands', `${type}.js`)
   const p = fork(processFile, { cwd })
 
@@ -16,8 +14,6 @@ const eventHandler = (type) => (event, mainWindow, session, props) => {
 
   p.send(Object.assign({}, { start: true }, system, props, { session: session.data }))
 }
-
-process.chdir(cwd)
 
 const startProduct = eventHandler('startProduct')
 const createProduct = require('./createProduct')(system)
