97395cf810e6dd1d1f1a2d4bfc82cdf73f974f8d
I. Dan Calinescu <idancalinescu@gmail.com>
2018-10-08 15:40:11 -0400

    created a smarter layout for the entire workspace content

--- chunks/studio/components/toolbar.js
+++ chunks/studio/components/toolbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Component, Components } from 'react-dom-chunky'
-import { Dropdown, Menu, Icon, Button } from 'antd'
+import { Dropdown, Menu, Icon } from 'antd'
+import { Button, ButtonIcon } from 'rmwc/Button'
+import { Fab } from 'rmwc/Fab'
+import { Elevation } from 'rmwc/Elevation'
 
 const SubMenu = Menu.SubMenu
 const ItemGroup = Menu.ItemGroup
@@ -11,10 +14,6 @@ export default class Toolbar extends Component {
 
     this.state = { }
     this._productOption = this.productOption.bind(this)
-    this._showAccountScreen = this.showAccountScreen.bind(this)
-    this._showTVScreen = this.showTVScreen.bind(this)
-    this._showBountiesScreen = this.showBountiesScreen.bind(this)
-    this._togglePreview = this.togglePreview.bind(this)
   }
 
   componentDidMount () {
@@ -25,24 +24,12 @@ export default class Toolbar extends Component {
     const [keyType, key] = item.key.split('/')
 
     switch (keyType) {
-      case 'product':
-        this.props.onProductChanged && this.props.onProductChanged(this.props.products[key])
+      case 'screen':
+        this.props.onScreenChanged && this.props.onScreenChanged(key)
         break
       default:
         this.props.onProductOption && this.props.onProductOption(key)
     }
-  }
-
-  showTVScreen () {
-    this.props.onShowTVScreen && this.props.onShowTVScreen()
-  }
-
-  showAccountScreen () {
-    this.props.onShowAccountScreen && this.props.onShowAccountScreen()
-  }
-
-  showBountiesScreen () {
-    this.props.onShowBountiesScreen && this.props.onShowBountiesScreen()
   }
 
   togglePreview () {
@@ -51,136 +38,130 @@ export default class Toolbar extends Component {
     this.setState({ preview })
   }
 
-  renderMenu (options) {
-    const products = Object.keys(this.props.products).map(p => <Menu.Item key={`product/${this.props.products[p].id}`} style={{ color: '#607D8B', margin: '0px', padding: '5px 15px 5px 5px' }}>
-      <Icon type='desktop' style={{ margin: '10px' }} />
-      { this.props.products[p].name }
-    </Menu.Item>)
+  trimString (s, max) {
+    const needsTrimming = name.length > max
+    return s.substring(0, needsTrimming ? max : s.length) + (needsTrimming ? '...' : '')
+  }
+
+  renderUserTitle () {
+    if (!this.props.account || !this.props.account.user) {
+      return 'Sign In'
+    }
+
+    return this.trimString(this.props.account.user.name, 16)
+  }
+
+  renderUserMenu (options) {
+    return <Menu onClick={this._productOption} style={{ padding: '0px'}}>
+      <Menu.Item key='screen/community' style={{ color: '#546E7A', margin: '0px', padding: '5px 15px 5px 5px' }}>
+        <Icon type='team' style={{ margin: '10px' }} />
+          Community Resources
+        </Menu.Item>
+      <Menu.Item key='screen/bounties' style={{ color: '#546E7A', margin: '0px', padding: '5px 15px 5px 5px' }}>
+        <Icon type='trophy' style={{ margin: '10px' }} />
+        Bounties
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key='screen/settings' style={{ color: '#546E7A', margin: '0px', padding: '5px 15px 5px 5px' }}>
+        <Icon type='setting' style={{ margin: '10px' }} />
+          Account Settings
+        </Menu.Item>
+    </Menu>
+  }
+
+  renderProductMenu (options) {
+    // const products = Object.keys(this.props.products).map(p => <Menu.Item key={`product/${this.props.products[p].id}`} style={{ color: '#607D8B', margin: '0px', padding: '5px 15px 5px 5px' }}>
+    //   <Icon type='desktop' style={{ margin: '10px' }} />
+    //   { this.props.products[p].name }
+    // </Menu.Item>)
 
     return <Menu onClick={this._productOption} style={{ padding: '0px'}}>
-      { products }
+      <Menu.Item key='/openFile' style={{ color: '#00bcd4', margin: '0px', padding: '5px 15px 5px 5px' }}>
+        <Icon type='file-text' style={{ margin: '10px' }} />
+        Open File
+      </Menu.Item>
+      <Menu.Item key='/publishProduct' style={{ color: '#00bcd4', margin: '0px', padding: '5px 15px 5px 5px' }}>
+        <Icon type='cloud-upload' style={{ margin: '10px' }} />
+        Publish Product
+      </Menu.Item>
       <Menu.Divider />
-      <Menu.Item key='/newProduct' style={{ color: '#42A5F5', margin: '0px', padding: '5px 15px 5px 5px' }}>
-        <Icon type='plus-circle' style={{ margin: '10px' }} />
-        Create New Product
+      <Menu.Item key='/switchProduct' style={{ color: '#00bcd4', margin: '0px', padding: '5px 15px 5px 5px' }}>
+        <Icon type='retweet' style={{ margin: '10px' }} />
+        Switch Product
       </Menu.Item>
     </Menu>
   }
 
-  renderMenuPrimary () {
-    return <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
-      <Dropdown
-        overlay={this.renderMenu()}
-        trigger={['click']}>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          marginLeft: '5px',
-          justifyContent: 'center'
-        }}>
-          <Button style={{
-            color: '#03A9F4'
-          }}>
-            { this.props.product.name }
-            <Icon type='down' style={{
-              color: '#03A9F4',
-              cursor: 'pointer'
-            }} />
-          </Button>
-        </div>
-      </Dropdown>
-    </div>
+  icon (type, action) {
+    return <Button
+      onClick={action}
+      style={{
+        color: '#546E7A',
+        textAlign: 'center',
+        padding: '0px',
+        justifyContent: 'center',
+        margin: '0px'
+      }}>
+      <ButtonIcon icon={type}
+        style={{
+          selfAlign: 'center',
+          padding: '0px',
+          margin: '0px'
+        }} />
+    </Button>
   }
 
   render () {
-    return <div style={{
+    return <Elevation z={2} style={{
       display: 'flex',
       flex: 1,
+      padding: '10px',
+      backgroundColor: '#ffffff',
+      width: '100%',
       flexDirection: 'row',
       alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      paddingRight: '10px'
+      height: '100%',
+      justifyContent: 'flex-start'
     }}>
-      <Icon
-        style={{
-          color: '#03A9F4',
-          display: 'flex',
-          fontSize: '18px',
-          marginLeft: '10px',
-          lineHeight: '64px',
-          padding: '0 10px',
-          cursor: 'pointer',
-          marginTop: '0px',
-          transition: 'color .3s'
-        }}
-        type={this.state.preview ? 'menu-unfold' : 'menu-fold'}
-        onClick={this._togglePreview} />
       <div style={{
-        flex: 1,
         display: 'flex',
-        height: '64px',
-        paddingTop: '2px',
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'flex-start',
         justifyContent: 'flex-start'
       }}>
-        <Button
-          shape='circle'
-          icon='export'
-          ghost
-          onClick={() => this._productOption({ key: '/publishProduct' })}
-          style={{
-            margin: '15px 0px 15px 0px',
-            color: '#03A9F4'
-          }} />
-        <Button
-          shape='circle'
-          icon='copy'
-          ghost
-          onClick={() => this._productOption({ key: '/openFile' })}
-          style={{
-            margin: '15px 0px 15px 0px',
-            color: '#03A9F4'
-          }} />
-        { this.renderMenuPrimary() }
+        { this.icon(this.state.preview ? 'phonelink' : 'phonelink_off', () => this.togglePreview()) }
+        <Dropdown
+          overlay={this.renderProductMenu()}
+          trigger={['hover']}>
+          <Button style={{
+            color: '#00bcd4',
+            marginLeft: '3px'
+          }}>
+            { this.trimString(this.props.product.name, 20) }
+            <ButtonIcon icon='expand_more' />
+          </Button>
+        </Dropdown>
       </div>
       <div style={{
         display: 'flex',
+        flexDirection: 'row',
         justifyContent: 'flex-end'
       }}>
-        <Button
-          shape='circle'
-          icon='play-circle'
-          ghost
-          onClick={this._showTVScreen}
-          style={{
-            margin: '15px 0px 15px 0px',
-            color: '#90A4AE'
-          }} />
-        <Button
-          shape='circle'
-          icon='gift'
-          ghost
-          onClick={this._showBountiesScreen}
-          style={{
-            margin: '15px 0px 15px 0px',
-            color: '#90A4AE'
-          }} />
-        <Button
-          shape='circle'
-          ghost
-          icon='user'
-          onClick={this._showAccountScreen}
-          style={{
-            margin: '15px 0px 15px 0px',
-            color: '#90A4AE'
-          }} />
+        <Dropdown
+          overlay={this.renderUserMenu()}
+          trigger={['hover']}>
+          <Button style={{
+            color: '#546E7A',
+            marginLeft: '3px'
+          }}>
+            <ButtonIcon icon='account_circle' />
+            { this.renderUserTitle() }
+            <ButtonIcon icon='expand_more' />
+          </Button>
+        </Dropdown>
       </div>
-    </div>
+    </Elevation>
   }
 
 }
--- chunks/studio/screens/index.desktop.js
+++ chunks/studio/screens/index.desktop.js
@@ -1,4 +1,4 @@
 export { default as new } from './new.desktop'
 export { default as workspace } from './workspace.desktop'
-export { default as tv } from './tv.desktop'
+export { default as community } from './community.desktop'
 export { default as bounties } from './bounties.desktop'
