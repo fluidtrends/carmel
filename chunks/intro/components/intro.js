4b2dfbfa13d8744a8ef155daafd025ebd16fdf22
aclowwwn <andi@fluidtrends.com>
2018-10-26 15:04:04 +0300

    Add video thumbnail

--- chunks/intro/components/intro.js
+++ chunks/intro/components/intro.js
@@ -78,16 +78,10 @@ export default class Intro extends Component {
   }
 
   main() {
-
-    const padding = this.props.isSmallScreen? '0 20px' : '100px 0 0 40px'
-
     return <div className="wharever" style={styles.colorful}>
-      <div style={{ opacity: 1.2, display: 'flex', flex: 2, justifyContent: 'center', padding }}>
-        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-          {this.title()}
-          {this.subtitle()}
-        </div>
-        {this.video()}
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        {this.title()}
+        {this.subtitle()}
       </div>
       {this.icons()}
       {}
@@ -95,8 +89,8 @@ export default class Intro extends Component {
   }
 
   title() {
-    const fontSize = 18
     const color = '#FAFAFA'
+    const fontSize = this.props.isSmallScreen? 25: 55
 
     return [<div >
       <h1 
@@ -104,7 +98,7 @@ export default class Intro extends Component {
         id="title" 
         style={{
           color,
-          fontSize: `${fontSize + 55}px`
+          fontSize: `${fontSize + fontSize}px`
         }}
       >
         Welcome to Carmel
@@ -112,33 +106,9 @@ export default class Intro extends Component {
     </div>]
   }
 
-  videoTitle() {
-    const fontSize = 18
-    const textWidth = 420
-    const color = '#fff'
-
-
-    if( this.props.isSmallScreen ) {
-      return <div />
-    }
-
-
-    return [<div xstyle={{ opacity: 1.2 }}>
-      <h2 key='title' style={{
-        color,
-        textAlign: 'center',
-        fontSize: `${fontSize + 30}px`,
-        textShadow: '2px 2px 5px #607D8B',
-        width: `${textWidth}px`
-      }}>
-        Get to know Carmel
-        </h2>
-    </div>]
-  }
-
   subtitle() {
-    const fontSize = 26
     const color = '#FAFAFA'
+    const fontSize = this.props.isSmallScreen? 15: 26
 
     return <Typist cursor={{ show: false }} style={{}}>
         <Typist.Delay ms={5000} />
@@ -172,44 +142,8 @@ export default class Intro extends Component {
     </Typist>
   }
 
-  distribution() {
-    const fontSize = 18
-    const color = '#FAFAFA'
-
-    return [<div style={{ opacity: 1, marginTop: 50 }}>
-      <h3 key='subtitle' style={{
-        color,
-        textAlign: 'center',
-        fontSize: `${fontSize + 10}px`
-      }}>
-        {distributedTokens}
-      </h3>
-    </div>]
-  }
-
-  video() {
-    const fontSize = 18
-    const textWidth = 420
-    const color = '#FAFAFA'
-
-    if( this.props.isSmallScreen ) {
-      return <div />
-    }
-
-    return <div style={{ display: 'flex', flexDirection: 'column', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <div style={{ marginTop: 75, width: 650, height: 350}}>
-          <Components.Media video={introVideo} width={650} height={350}/>
-      </div>
-      {this.videoTitle()}
-    </div>
-  }
-
   icons() {
-    const fontSize = 18
-    const textWidth = 420
-    const color = '#FAFAFA'
-
-    return [<div style={{ opacity: 1.2, display: 'flex', justifyContent: 'flex-end', paddingRight: 100, paddingBottom: 20 }}>
+    return [<div style={{ opacity: 1.2, display: 'flex', justifyContent: 'flex-end', paddingRight: 100, paddingBottom: 30 }}>
       {this.renderIcons()}
     </div>]
   }
@@ -230,8 +164,9 @@ export default class Intro extends Component {
     ]
 
     const align = this.props.isSmallScreen ? 'center' : 'center'
-    const { social } = this.props
     const overflow = this.props.isSmallScreen ? 'auto' : 'unset'
+    const fontSize = this.props.isSmallScreen? 20: 36
+    const padding = this.props.isSmallScreen? 6: 10
 
     return <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', alignSelf: align, overflow }}>
       {socialNetworks.map(key => {
@@ -245,8 +180,8 @@ export default class Intro extends Component {
             onClick={this.goto.bind(this, key)}
             style={{
               cursor: 'pointer',
-              fontSize: 36,
-              padding: '10px'
+              fontSize,
+              padding
             }} />
           <style jsx>{`
               div :global(.icon) {
@@ -261,9 +196,6 @@ export default class Intro extends Component {
   }
 
   render () {
-    // const smallScreenLayout = [...this.sky(this.props.offset, 0),
-    // ...this.title(this.props.offset + 0.1, 0.2),
-    // ...this.timer(this.props.offset + 0.29, 0.2)]
     return [
       ...this.main()
     ]
