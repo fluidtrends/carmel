4faf29755c352e07a85e005bdc08494f6b4a39be
aclowwwn <andi@fluidtrends.com>
2018-10-29 16:25:43 +0200

    Update chart

--- chunks/intro/components/chart.js
+++ chunks/intro/components/chart.js
@@ -3,16 +3,13 @@ import { Component } from 'react-dom-chunky'
 import { PieChart, LabelList, Pie, Sector, Cell } from 'recharts'
 
 const data = [
-  { name: 'Private Sale', value: 3.5, id: '0', percent: '5%' },
-  { name: 'AirDrop', value: 3.5, id: '1', percent: '5%' },
-  { name: 'Pre-Sale', value: 7, id: '1', percent: '10%' },
-  { name: 'Public Sale', value: 28, id: '2', percent: '40%' },
-  { name: 'Fluid Fund', value: 7, id: '3', percent: '10%' },
-  { name: 'Founders Fund', value: 14, id: '4', percent: '20%' },
-  { name: 'Partners Fund', value: 7, id: '5', percent: '10%' }
+  { name: 'Community', value: 6, id: '0', percent: '60%%' },
+  { name: 'Founders Fund', value: 2, id: '1', percent: '20%' },
+  { name: 'Business Partnerships', value: 1, id: '2', percent: '10%' },
+  { name: 'Bounties', value: 1, id: '3', percent: '10%' }
 ]
 
-const COLORS = ['#66BB6A', '#43A047', '#2E7D32', '#00796B', '#1976D2', '#1A237E', '#0D47A1']
+const COLORS = ['#66BB6A', '#00796B', '#1976D2', '#1A237E', '#0D47A1']
 const RADIAN = Math.PI / 180
 
 export default class ChartComponent extends Component {
