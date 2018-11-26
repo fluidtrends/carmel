4347b3a0d61521555e092673d0bfac71ad0b34ad
I. Dan Calinescu <idancalinescu@gmail.com>
2018-11-26 18:11:36 -0500

    added preliminary credits and story chunks

---
+++ chunks/wallet/components/credits.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Component } from 'react-dom-chunky'
+import { Typography } from '@rmwc/typography'
+import { Chip, ChipText, ChipIcon, ChipSet } from '@rmwc/chip'
+import {
+  Card,
+  CardPrimaryAction,
+  CardMedia,
+  CardAction,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons
+} from '@rmwc/card'
+import { Row, Col } from 'antd'
+import { Button, ButtonIcon } from '@rmwc/button'
+import { Icon } from '@rmwc/icon';
+import Items from '../data/credits.json'
+
+export default class Credits extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { ...super.state }
+  }
+
+  componentDidMount () {
+    super.componentDidMount()
+  }
+
+  get items() {
+    return Items
+  }
+
+  renderCard(item) {
+    return <Card style={{  margin: "10px" }}>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick"/>
+        <input type="hidden" name="hosted_button_id" value={item.paymentId}/>
+
+          <CardPrimaryAction style={{ justifyContent: "center", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <div style={{ padding: '10px', marginTop: "20px" }}>
+              <Typography use="headline6" tag="h1">
+                { item.title }
+              </Typography>
+              <Typography
+                use="headline7"
+                tag="div"
+                theme="text-secondary-on-background"
+                style={{ margin: '1rem', textAlign: "center" }}>
+                { item.subtitle }
+              </Typography>
+            </div>
+            <CardActions style={{ justifyContent: "center" }}>
+              <Button
+                type="submit"
+                style={{
+                  margin: "10px 0px 30px 0px",
+                  color: '#ffffff',
+                  backgroundColor: `${item.color}`
+                }}>
+                <ButtonIcon icon="check_circle" style={{ marginLeft: "5px"}}/>
+                { item.action }
+                <ButtonIcon icon="arrow_forward" style={{ marginLeft: "5px"}}/>
+              </Button>
+            </CardActions>
+          </CardPrimaryAction>
+        </form>
+      </Card>
+  }
+
+  render() {
+    return <div
+      style={{
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: this.props.compact ? 'column' : 'row',
+        alignItems: 'center'
+      }}>
+        { this.items.map(item => this.renderCard(item))}
+    </div>
+  }
+}
--- chunks/wallet/screens/main.web.js
+++ chunks/wallet/screens/main.web.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Screen, Components } from 'react-dom-chunky'
+import { Typography } from '@rmwc/typography'
+import Credits from '../components/credits'
 
-export default class MainWalletScreen extends Screen {
+export default class CreditsScreen extends Screen {
   constructor (props) {
     super(props)
   }
 
+  renderMainContent() {
+      return (<div
+        style={{
+          display: 'flex',
+          flex: 1,
+          padding: "10px",
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+        <Credits compact={this.isSmallScreen}/>
+      </div>)
+  }
+
   components () {
+    return [this.renderMainContent()]
   }
 }
