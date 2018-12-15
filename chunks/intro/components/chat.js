f68f20215504c7bf94c5b7d206c3d6e1d007c227
I. Dan Calinescu <idancalinescu@gmail.com>
2018-12-15 01:59:43 -0500

    removing the bot launcher for now

--- chunks/intro/components/chat.js
+++ chunks/intro/components/chat.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Component } from 'react-dom-chunky'
-import {
-  Widget,
-  addResponseMessage,
-  addLinkSnippet,
-  addUserMessage
-} from 'react-chat-widget'
+// import {
+//   Widget,
+//   addResponseMessage,
+//   addLinkSnippet,
+//   addUserMessage
+// } from 'react-chat-widget'
 import Wobble from 'react-reveal/Wobble'
 
 export default class Chat extends Component {
@@ -15,48 +15,53 @@ export default class Chat extends Component {
   }
   componentDidMount() {
     super.componentDidMount()
-    addResponseMessage('How may I assist you ?')
+    // addResponseMessage('How may I assist you ?')
   }
 
-  handleNewUserMessage = newMessage => {
-    console.log(`New message incoming! ${newMessage}`)
-    // resp message
-    // if (respMessage.includes('https://'))
-    //   addLinkSnippet({
-    //     title: 'telegram',
-    //     link: 'https://t.me/carmelplatform'
-    //   })
-  }
-
-  renderLauncher(handleToggle) {
-    return (
-      <div
-        onMouseEnter={() => this.setState({ hover: !this.state.hover })}
-        style={{ display: 'flex', justifyContent: 'flex-end' }}
-      >
-        <Wobble spy={this.state.hover}>
-          <img
-            onClick={handleToggle}
-            src="assets/chunky-logo.gif"
-            style={{ cursor: 'pointer', width: 100, height: 100 }}
-          />
-        </Wobble>
-      </div>
-    )
-  }
+  // handleNewUserMessage = newMessage => {
+  //   console.log(`New message incoming! ${newMessage}`)
+  //   // resp message
+  //   // if (respMessage.includes('https://'))
+  //   //   addLinkSnippet({
+  //   //     title: 'telegram',
+  //   //     link: 'https://t.me/carmelplatform'
+  //   //   })
+  // }
 
   render() {
-    return (
-      <Widget
-        title={'Hello I am chunky'}
-        subtitle={
-          'I am your code monkey which will assist you in the tech jungle'
-        }
-        handleNewUserMessage={this.handleNewUserMessage}
-        badge={1}
-        showCloseButton={true}
-        launcher={handleToggle => this.renderLauncher(handleToggle)}
-      />
-    )
+    return <div/>
   }
+
+
+  // renderLauncher(handleToggle) {
+  //   return (
+  //     <div
+  //       onMouseEnter={() => this.setState({ hover: !this.state.hover })}
+  //       style={{ display: 'flex', justifyContent: 'flex-end' }}
+  //     >
+  //       <Wobble spy={this.state.hover}>
+  //         <img
+  //           onClick={handleToggle}
+  //           src="assets/chunky-logo.gif"
+  //           style={{ cursor: 'pointer', width: 100, height: 100 }}
+  //         />
+  //       </Wobble>
+  //     </div>
+  //   )
+  // }
+
+  // render() {
+  //   return (
+  //     <Widget
+  //       title={'Hello I am chunky'}
+  //       subtitle={
+  //         'I am your code monkey which will assist you in the tech jungle'
+  //       }
+  //       handleNewUserMessage={this.handleNewUserMessage}
+  //       badge={1}
+  //       showCloseButton={true}
+  //       launcher={handleToggle => this.renderLauncher(handleToggle)}
+  //     />
+  //   )
+  // }
 }
