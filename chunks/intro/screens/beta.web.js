c65c22b86989d9267f19f85d5de05c5f1a13ed5c
aclowwwn <andi@fluidtrends.com>
2019-01-08 22:12:41 +0200

    Add download button

--- chunks/intro/screens/beta.web.js
+++ chunks/intro/screens/beta.web.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import { Screen, Components } from 'react-dom-chunky'
+const OSVersions = {
+  "MacIntel": "Mac",
+  "Win32": "Windows",
+  "Linux": "Linux"
+}
+const StudioVersions = {
+  "MacIntel": "http://files.carmel.io/studio/Carmel-Beta01.dmg",
+  "Win32": "http://files.carmel.io/studio/Carmel.7z",
+  "Linux": "http://files.carmel.io/studio/Carmel.7z"
+}
 
 export default class MainPersonScreen extends Screen {
   constructor (props) {
@@ -12,6 +22,18 @@ export default class MainPersonScreen extends Screen {
   }
 
   renderScreenshots () {
+  }
+
+  get cover () {
+    let platform = 'MacIntel'
+    if (window && window.navigator) {
+      platform = window.navigator.platform.split(' ')[0]
+    }
+
+    return Object.assign({}, this.props.cover, {
+      primaryActionTitle: `Download the studio for ${OSVersions[platform]}`,
+      link: StudioVersions[platform]
+    })
   }
 
   get features () {
