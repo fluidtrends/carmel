b33897073cc934ea61c492407d087ecaf06bf611
aclowwwn <andi@fluidtrends.com>
2019-01-09 16:10:35 +0200

    Add journey page

---
+++ chunks/journey/screens/main.web.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Screen, Components } from 'react-dom-chunky'
+
+export default class MainChallengesScreen extends Screen {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ...this.state
+    }
+  }
+
+  componentDidMount() {
+    super.componentDidMount()
+    this._username = this.props.location.pathname.split('/')[2]
+    this.setState({
+    })
+  }
+
+  get cover () {
+    return Object.assign({}, this.props.cover, {
+      title: `${this.username}'s #CarmelStory`
+    })
+  }
+
+  get username () {
+    return this._username
+  }
+
+
+  components() {
+    return super.components().concat([])
+  }
+}
