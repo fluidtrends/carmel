9831779d1efb385c788318610c88a6b9a52b82b3
Robert Pop <poprobertdaniel22@gmail.com>
2019-01-10 11:04:40 +0200

    clean up playground

--- chunks/challenges/components/challengePlayground.js
+++ chunks/challenges/components/challengePlayground.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Component, Components } from 'react-dom-chunky'
 import { Row, Col, Button, Icon } from 'antd'
 import Task from './playground/task'
-import TaskCard from './playground/taskCard'
+
 import Editor from './playground/editor'
 import ChunkyProduct from './playground/product'
 
@@ -10,22 +10,11 @@ export default class ChallengePlayground extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      startedTask: false,
-      selectedTask: null
-    }
+    this.state = {}
   }
 
   componentDidMount() {
     super.componentDidMount()
-  }
-
-  selectTask = selectedTask => {
-    this.setState({ selectedTask, startedTask: true })
-  }
-
-  resetTask = () => {
-    this.setState({ startedTask: false, selectTask: null })
   }
 
   handleEditorChange = val => {
@@ -70,83 +59,6 @@ export default class ChallengePlayground extends Component {
           Verify Task
         </Button>
       </div>
-    )
-  }
-
-  renderInitial() {
-    const { challenge, defaults, initial } = this.props
-    const { taskIds, id } = challenge
-    const { newValues } = this.state
-
-    const columnStyle = { padding: '20px' }
-    return (
-      <React.Fragment>
-        <Row style={{ margin: '10px 20px' }}>
-          <Col style={columnStyle} span={12} offset={6}>
-            <Task
-              task={require(`../../../challenges/${id}/${
-                taskIds[0]
-              }/index.json`)}
-            />
-          </Col>
-        </Row>
-        <Row
-          style={{
-            margin: '10px 20px',
-            display: 'flex',
-            alignItems: 'center'
-          }}
-        >
-          <Col style={columnStyle} span={10}>
-            <Editor
-              value={JSON.stringify(
-                this.state.newValues ? this.state.newValues : defaults,
-                null,
-                '\t'
-              )}
-              onChange={val => this.handleEditorChange(val)}
-              mode="json"
-              theme="monokai"
-              name="editor"
-              fontSize={14}
-              style={{ width: '100%' }}
-              showPrintMargin={true}
-              showGutter={true}
-              highlightActiveLine={true}
-              setOptions={{
-                showLineNumbers: true,
-                tabSize: 2
-              }}
-            />
-          </Col>
-          <Col style={columnStyle} span={14}>
-            {defaults && (
-              <ChunkyProduct
-                source={(newValues && newValues.source) || defaults.source}
-                image={(newValues && newValues.image) || defaults.image}
-                opacity={(newValues && newValues.opacity) || defaults.opacity}
-                title={(newValues && newValues.title) || defaults.title}
-                type={(newValues && newValues.type) || defaults.type}
-                subtitle={
-                  (newValues && newValues.subtitle) || defaults.subtitle
-                }
-                titleStyle={
-                  (newValues && newValues.titleStyle) || defaults.titleStyle
-                }
-                subtitleStyle={
-                  (newValues && newValues.subtitleStyle) ||
-                  defaults.subtitleStyle
-                }
-              />
-            )}
-          </Col>
-        </Row>
-        <Row style={{ margin: '10px 20px' }}>
-          <Col style={columnStyle} span={12} offset={6}>
-            {this.renderButtons()}
-          </Col>
-        </Row>
-      </React.Fragment>
     )
   }
 
