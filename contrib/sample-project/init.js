ac0f890216bdfbeaad2b2f6eb58c20a2cc15dbe8
I. Dan Calinescu <idancalinescu@gmail.com>
2019-01-29 10:42:36 -0500

    added sample project

--- contrib/sample-project/init.js
+++ contrib/sample-project/init.js
@@ -1,5 +1,3 @@
-const { expect, assert, should } = chai
-
 const REQUIREMENTS = [
   `at least a chunk with a route that contains a title, a cover title and a cover subtitle`
 ]
@@ -8,16 +6,7 @@ const validRoute = (route) => {
   return route && route.title && route.cover && route.cover.title && route.cover.subtitle
 }
 
-const initialChecks = () => {
-  expect(carmel).to.exist
-  expect(carmel.product).to.exist
-  expect(carmel.product.chunks).to.be.an('array').that.is.not.empty
-  expect(carmel.utils).to.exist
-  expect(carmel.task).to.exist
-  expect(carmel.challenge).to.exist
-}
-
-const findChunksWithValidRoutes = () => {
+const findChunksWithValidRoutes = (carmel) => {
   return carmel.product.chunks.map(chunk => {
     if (!chunk.config.routes) {
       return
@@ -28,15 +17,15 @@ const findChunksWithValidRoutes = () => {
   })
 }
 
-const init = (done) => {
-  // Perform basic checks
-  initialChecks()
-
+const init = (carmel) => new Promise((resolve, reject) => {
   // Let's look up chunks with at least a route that contains a title, a cover title and a cover subtitle
-  const chunks = findChunksWithValidRoutes()
+  const chunks = findChunksWithValidRoutes(carmel)
 
-  // This challenge needs a route that contains a title, a cover title and a cover subtitle
-  assert(chunks && chunks.length > 0, `This challenge requires ${REQUIREMENTS[0]}`)
+  if (!chunks || chunks.length === 0) {
+    // This challenge needs a route that contains a title, a cover title and a cover subtitle
+    reject(new Error(`This challenge requires ${REQUIREMENTS[0]}`))
+    return
+  }
 
   // Let's take the first of these
   const chunk = chunks[0]
@@ -44,7 +33,7 @@ const init = (done) => {
   // And let's also take its first valid route
   const route = chunk.validRoutes[0]
 
-  done ({ chunk, route })
-}
+  resolve({ chunk, route })
+})
 
 module.exports = init
