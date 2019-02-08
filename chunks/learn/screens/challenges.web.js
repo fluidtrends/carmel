0fb7a4629a98cf039425b374d1c545e0504e5bae
Robert Pop <poprobertdaniel22@gmail.com>
2019-02-08 21:00:15 +0200

    future challenges

--- chunks/learn/screens/challenges.web.js
+++ chunks/learn/screens/challenges.web.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Screen, Components } from 'react-dom-chunky'
 import { Row, Col, Radio, Tag, Avatar, Alert } from 'antd'
 import ChallengeCard from '../components/challengeCard'
+import FutureChallengeCard from '../components/futureChallengeCard'
 import Challenge from '../components/Challenge'
 import { Data } from 'react-chunky'
 import merge from 'deepmerge'
@@ -21,10 +22,23 @@ export default class MainChallengesScreen extends Screen {
     super.componentDidMount()
 
     this.refreshContent()
+    this._getFutureChallenges()
   }
 
   refreshContent() {
-    this.props.getListings(this.dynamicVariant ? { challengeId: this.dynamicVariant } : { all: true })
+    this.props.getListings(
+      this.dynamicVariant ? { challengeId: this.dynamicVariant } : { all: true }
+    )
+  }
+
+  _getFutureChallenges() {
+    const self = this
+
+    this.importRemoteData(this.props.futureChallenges)
+      .then(futureChallengesData => {
+        self.setState({ futureChallengesData })
+      })
+      .catch(error => console.log(error))
   }
 
   gotListings(content) {
@@ -34,10 +48,12 @@ export default class MainChallengesScreen extends Screen {
       return
     }
 
-
     if (this.dynamicVariant && content.data.challenge) {
       if (this.isLoggedIn) {
-        this.updateWorkspace({ challenge: content.data.challenge, event: "startChallenge" })
+        this.updateWorkspace({
+          challenge: content.data.challenge,
+          event: 'startChallenge'
+        })
         return
       }
 
@@ -45,14 +61,15 @@ export default class MainChallengesScreen extends Screen {
       return
     }
 
-
     if (!content.data.challenges) {
       this.setState({ challenges: [] })
       return
     }
 
     // Only look for published challenges
-    const challenges = content.data.challenges.filter(c => c.status === 'published')
+    const challenges = content.data.challenges.filter(
+      c => c.status === 'published'
+    )
     this.setState({ challenges })
   }
 
@@ -129,7 +146,8 @@ export default class MainChallengesScreen extends Screen {
       })
     }
 
-    return [<div>
+    return [
+      <div>
         <Components.Text
           source={'local://challenges-intro'}
           style={{
@@ -149,92 +167,122 @@ export default class MainChallengesScreen extends Screen {
         )}
         {this.renderTags()}
         <Row gutter={26} style={{ padding: '20px' }}>
-            <React.Fragment>
-              {filteredChallenges.length ? (
-                filteredChallenges.map(challenge => (
-                  <div
-                    style={{
-                      padding: '20px',
-                      maxWidth: '700px',
-                      margin: '0 auto'
-                    }}
-                  >
-                    <ChallengeCard
-                      challenge={challenge}
-                      onSelectChallenge={selectedChallenge =>
-                        this.props.history.push(
-                          `/challenges/${selectedChallenge.id}`
-                        )
-                      }
-                      onCategoryClick={category => this.addCategory(category)}
-                    />
-                  </div>
-                ))
-              ) : (
-                <Col
-                  span={16}
-                  offset={4}
-                  style={{ padding: '20px', height: '475px' }}
+          <React.Fragment>
+            {filteredChallenges.length ? (
+              filteredChallenges.map(challenge => (
+                <div
+                  style={{
+                    padding: '20px',
+                    maxWidth: '700px',
+                    margin: '0 auto'
+                  }}
                 >
-                  <p>Sorry we can't find the challenges you are looking for.</p>
-                </Col>
-              )}
-            </React.Fragment>
+                  <ChallengeCard
+                    challenge={challenge}
+                    onSelectChallenge={selectedChallenge =>
+                      this.props.history.push(
+                        `/challenges/${selectedChallenge.id}`
+                      )
+                    }
+                    onCategoryClick={category => this.addCategory(category)}
+                  />
+                </div>
+              ))
+            ) : (
+              <Col
+                span={16}
+                offset={4}
+                style={{ padding: '20px', height: '475px' }}
+              >
+                <p>Sorry we can't find the challenges you are looking for.</p>
+              </Col>
+            )}
+            {this.state.futureChallengesData &&
+              this.state.futureChallengesData.map(challenge => (
+                <div
+                  style={{
+                    padding: '20px',
+                    maxWidth: '700px',
+                    margin: '0 auto'
+                  }}
+                >
+                  <FutureChallengeCard challenge={challenge} />
+                </div>
+              ))}
+          </React.Fragment>
         </Row>
-      </div>]
+      </div>
+    ]
   }
 
   joinNow(session) {
-    Data.Cache.cacheItem("guestSession", Object.assign({}, session, {
-      guild: "learners",
-      alert: `You will need to sign in before you can start the "${this.state.challenge.title}" challenge`,
-      workspace: { challenge: this.state.challenge, event: "startChallenge" }
-    }))
-    .then(() => this.triggerRedirect('/register'))
-    .catch((e) => this.triggerRedirect('/register'))
+    Data.Cache.cacheItem(
+      'guestSession',
+      Object.assign({}, session, {
+        guild: 'learners',
+        alert: `You will need to sign in before you can start the "${
+          this.state.challenge.title
+        }" challenge`,
+        workspace: { challenge: this.state.challenge, event: 'startChallenge' }
+      })
+    )
+      .then(() => this.triggerRedirect('/register'))
+      .catch(e => this.triggerRedirect('/register'))
   }
 
   join() {
-    Data.Cache.retrieveCachedItem("guestSession")
-              .then((session) => this.joinNow(session))
-              .catch((e) => this.joinNow())
+    Data.Cache.retrieveCachedItem('guestSession')
+      .then(session => this.joinNow(session))
+      .catch(e => this.joinNow())
   }
 
   _doUpdateWorkspace(data) {
-    Data.Cache.cacheItem("workspace", data)
-              .then(() => this.triggerRedirect('/me/workspace'))
-              .catch((e) => this.triggerRedirect('/me/workspace'))
+    Data.Cache.cacheItem('workspace', data)
+      .then(() => this.triggerRedirect('/me/workspace'))
+      .catch(e => this.triggerRedirect('/me/workspace'))
   }
 
   updateWorkspace(data) {
-    Data.Cache.retrieveCachedItem("workspace")
-              .then((workspace) => this._doUpdateWorkspace(merge.all([workspace, data])))
-              .catch((e) => this._doUpdateWorkspace(data))
+    Data.Cache.retrieveCachedItem('workspace')
+      .then(workspace => this._doUpdateWorkspace(merge.all([workspace, data])))
+      .catch(e => this._doUpdateWorkspace(data))
   }
 
-  startChallenge () {
-    if (!this.isLoggedIn && this.state.challenge.type === "Professional") {
+  startChallenge() {
+    if (!this.isLoggedIn && this.state.challenge.type === 'Professional') {
       this.join()
       return
     }
   }
 
   renderChallenge() {
-    return [<div style={{flex: 1, display: "flex", alignItems: "center"}}>
-            <Avatar src="/assets/chunky-logo.gif" style={{
-              height: "180px", width: "180px"
-            }} /></div>,
-        <Challenge
+    return [
+      <div style={{ flex: 1, display: 'flex', alignItems: 'center' }}>
+        <Avatar
+          src="/assets/chunky-logo.gif"
+          style={{
+            height: '180px',
+            width: '180px'
+          }}
+        />
+      </div>,
+      <Challenge
         challenge={this.state.challenge}
         onSelectChallenge={this._startChallenge}
-     />]
+      />
+    ]
   }
 
   components() {
-    if (this.state.inProgress || (!this.state.challenges && !this.state.challenge)) {
-      return [<Components.Loading message="Just a sec, please"/>]
+    if (
+      this.state.inProgress ||
+      (!this.state.challenges && !this.state.challenge)
+    ) {
+      return [<Components.Loading message="Just a sec, please" />]
     }
 
-    return (this.state.challenge ? this.renderChallenge() : this.renderChallenges())
+    return this.state.challenge
+      ? this.renderChallenge()
+      : this.renderChallenges()
   }
 }
