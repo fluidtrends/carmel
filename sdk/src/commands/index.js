79a0ac88aa2aada196bb92e76fd95440d3167dab
I. Dan Calinescu <idancalinescu@gmail.com>
2020-03-21 17:36:06 +0200

    [sdk] #641: added preview command

--- sdk/src/commands/index.js
+++ sdk/src/commands/index.js
@@ -2,10 +2,12 @@ const Init = require('./init')
 const Install = require('./install')
 const Start = require('./start')
 const Make = require('./make')
+const Preview = require('./preview')
 
 module.exports = {
   Init,
   Install,
   Start,
+  Preview,
   Make
 }
