2380f0aecb1aa1e207952b384de5a521789c406d
I. Dan Calinescu <idancalinescu@gmail.com>
2020-04-13 21:29:07 +0300

    added tight awsome cloud integration

--- sdk/src/commands/cloud/index.js
+++ sdk/src/commands/cloud/index.js
@@ -9,13 +9,41 @@ class _ extends Command {
   get requiresContext() { return _.REQUIRES_CONTEXT }
   get title() { return _.TITLE }
 
+  status(session) {
+    session.logger.info("Checking the status of your cloud ...")
+
+    return this.prepareCloud(session)
+               .then(() => this.cloud.status())
+  }
+
+  init(session) {
+    session.logger.info("Setting up your cloud ...")
+
+    return this.prepareCloud(session)
+               .then(() => this.cloud.init())
+  }
+
+  push(session) {
+    session.logger.info("Deploying cloud changes ...")
+
+    return this.prepareCloud(session)
+               .then(() => this.cloud.push())
+  }
+
   exec(session) {
     if (!session) {
       return Promise.reject(new Error(_.ERRORS.COULD_NOT_EXECUTE('the session is missing')))
     }
 
-    return this.prepareCloud(session)
-               .then(() => this.cloud.init())
+    if (!this.args.operation) {
+      return this.status(session)
+    }
+
+    if (!this[this.args.operation]) {
+      return this.status(session)
+    }
+
+    return this[this.args.operation](session)
   }
 }
 
