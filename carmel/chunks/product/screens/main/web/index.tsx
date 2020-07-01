909290330cab9c1bfb17a485da83675ebdacfdb3
I. Dan Calinescu <idancalinescu@gmail.com>
2020-07-01 08:10:35 +0300

    updated basic docs

--- carmel/chunks/product/screens/main/web/index.tsx
+++ carmel/chunks/product/screens/main/web/index.tsx
@@ -2,5 +2,9 @@ import React, { Fragment } from 'react'
 import { Web } from 'jayesse'
 
 export default () => {
-  return <Web.Components.Document />
+  return <Web.Components.Document 
+          repo="fluidtrends/carmel"
+          branch="master"
+          root="docs"
+          mount="/docs"/>
 }
