45da52a957a9e8896fa78f878efa75505f9a35a2
I. Dan Calinescu <dan@fluidtrends.ch>
2022-11-28 01:42:35 +0100

    added gradient fonts

--- web/components/hero/index.tsx
+++ web/components/hero/index.tsx
@@ -5,11 +5,12 @@ const Video = () => (
 )
 
 const Text = () => (
-   <div className="relative z-30 p-5 bg-black text-2xl text-white bg-purple-300 bg-opacity-50 h-full grid place-items-center h-screen">
-     <div className="pb-40">
-        <h1 className="mb-5 lg:text-5xl text-xl font-bold">
-            Imagine a more human world
+    <div className="relative w-full z-30 p-5 bg-black text-2xl text-white bg-purple-300 bg-opacity-50 h-full grid place-items-center h-screen">
+      <div className="pb-40">
+        <h1 className="mb-5 lg:text-6xl text-4xl bg-gradient-to-r from-primary-color via-accent-color to-white inline-block text-transparent bg-clip-text">
+              Imagine a more human world
         </h1>
+
         <p className="lg:text-xl text-sm">
           Where technology serves and does not dominate human experiences
         </p>
@@ -18,7 +19,7 @@ const Text = () => (
 )
 
 export default () => (
-  <div className="relative flex items-center justify-center h-screen overflow-hidden border-b border-primary-color">
+  <div className="relative flex items-center w-full justify-center h-screen overflow-hidden border-b border-primary-color">
     <Text/>
     <Video/>
 </div>
