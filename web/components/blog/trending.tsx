22dff7512e71cd0e1d0eb93c332a4b6ea974a441
I. Dan Calinescu <dan@fluidtrends.ch>
2023-01-13 15:23:58 +0100

    fixed compile errors

--- web/components/blog/trending.tsx
+++ web/components/blog/trending.tsx
@@ -1,11 +1,11 @@
 import RelatedPost from '~/components/blog/related'
-import type { Posts } from './props'
+import type { PostsProps } from './props'
 
-export default ({ posts }: Posts) => (
+export default ({ posts }: PostsProps) => (
   <div className="flex flex-col flex-grow w-full lg:w-auto">
     <h3 className="font-epilogue-bold text-5xl">Trending</h3>
     <div className="flex flex-col justify-between h-full w-full items-start lg:gap-0 gap-6">
-      { posts.slice(3, 7).map((post, i) => (
+      { posts.slice(3, 7).map((post: any, i) => (
         <RelatedPost
           key={post.slug}
           {...post}
