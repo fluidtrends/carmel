7d84c0b76155975629c3fdf4755994df7cbdd600
I. Dan Calinescu <dan@fluidtrends.ch>
2023-01-13 15:42:23 +0100

    updated landing

--- web/screens/public/home/index.tsx
+++ web/screens/public/home/index.tsx
@@ -2,13 +2,12 @@ import Container from '~/containers/public'
 import Hero from '~/components/hero'
 import Features from '~/components/features'
 
-import Dashboard from '~/screens/private/dashboard'
+// import Dashboard from '~/screens/private/dashboard'
+// export default Dashboard
 
-export default Dashboard
-
-// export default () => (
-//   <Container>
-//       <Hero/>
-//       <Features/>
-//   </Container>
-// )
+export default () => (
+  <Container>
+      <Hero/>
+      <Features/>
+  </Container>
+)
